108e7b37c2c2bab0dafe8299ca947a302332aae3
Caio medeirso <caio.medeiros@segurosicoob.com>
2022-12-06 13:52:12 -0300

    Adicionar arquivos de projeto.

---
+++ lol-auto-accept/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace lol_auto_accept
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(110, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Start Auto Accept";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 270);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Auto Accept LoL";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ lol-auto-accept/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AutoItX3Lib;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace lol_auto_accept
+{
+    public partial class Form1 : Form
+    {
+        IntPtr handle;
+        string WINDOW_NAME = "League of Legends";
+
+        AutoItX3 au3 = new AutoItX3();
+
+        public RECT rect;
+
+        public struct RECT
+        {
+            public int left, top,  right, bottom;
+        }
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr FindWindow(string IpClassName, string IpWindowName);
+
+        [DllImport("user32.dll")]
+        static extern bool GetWindowRect(IntPtr hWnd, out RECT IpRect);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread AA = new Thread(auto_accept) { IsBackground = true };
+            AA.Start();
+        }
+
+        private void auto_accept()
+        {
+            while (true)
+            {
+                if (checkBox1.Checked)
+                {
+                    getWindowRectangle();
+                    int col = au3.PixelGetColor(rect.left + 584, rect.top + 555);
+                    if (col == 0x1E252A)
+                    {
+                        Thread.Sleep(20);
+                        au3.MouseMove(rect.left + 584, rect.top + 555, 1);
+                        au3.MouseClick("LEFT");
+                        Thread.Sleep(20);
+                        au3.MouseClick("LEFT");
+                        Thread.Sleep(5000);
+                    }
+                }
+                Thread.Sleep(20);
+            }
+        }
+
+        void getWindowRectangle()
+        {
+            handle = FindWindow(null, WINDOW_NAME);
+            GetWindowRect(handle, out rect);
+        }
+    }
+}
